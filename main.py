282e897bea6a70e926f35e5c7f385ca6630c5095
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2025-09-15 13:03:32 +0000

    up

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# pip install -r requirements.txt
 
 from os import name
 app = Flask (__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from models import *
 from routes import *
     
 if __name__ == "__main__":
