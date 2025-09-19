5c9dfdf2ddc8df7304523553716bd0c9e5f3ff51
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2025-09-19 22:54:01 +0000

    up forms

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import User
+
+class RegistrationForm(FlaskForm):
+    nome = StringField('Nome',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    sobrenome = StringField('Sobrenome',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmar_senha = PasswordField('Confirmar Senha',
+                                     validators=[DataRequired(), EqualTo('senha')])
+    submit = SubmitField('Registrar')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Este email já está em uso. Por favor, escolha outro.')
