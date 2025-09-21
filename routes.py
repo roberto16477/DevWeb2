c6f34ffff1c75d67fd53709e39458730549462bc
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2025-09-21 19:11:59 +0000

    atts

--- routes.py
+++ routes.py
@@ -10,7 +10,7 @@ from forms import RegistrationForm, LoginForm
 #rotas
 @app.route("/")
 def homepage():
-    return render_template('homepage.html', title='Página Inicial')
+    return render_template("Bem vindo ao blog")
 
 @app.route("/blog")
 def blog():
